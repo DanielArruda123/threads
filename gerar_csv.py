c8905d296a5e74ddd8479889e00840f94d0b3037
Daniel <daniel333arruda@gmail.com>
2025-11-05 20:12:21 -0300

    dar reset1

--- gerar_csv.py
+++ gerar_csv.py
@@ -1,7 +1,7 @@
 # gera_csv.py
 import csv
 import os
-#qualquer coisa aquiaaaaaaa
+#qualquer coisa aquiaaaa
 def gerar_csv_dados(filepath):
     """Gera arquivo CSV com dados de teste"""
     # Garante que o diretório existe
